2bce1b83c370402019b959a592bccb62585ea978
Maxye4655 <bowmakermax@gmail.com>
2026-08-21 22:56:11 +1200

    Add hostname discovery

--- netwatch.py
+++ netwatch.py
@@ -1,34 +1,42 @@
-import socket
 import ipaddress
+import socket
 from scapy.all import ARP, Ether, srp
 
 
-#scan the network for active devices
+def get_hostname(ip):
+  try:
+    hostname = socket.gethostbyaddr(ip)[0]
+    return hostname
+  except socket.herror:
+    return "Unknown"
+
+
+# scan the network for active devices
 def scan_network(network):
-    arp_request = ARP(pdst=str(network))
-    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
-    packet = broadcast / arp_request
+  arp_request = ARP(pdst=str(network))
+  broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+  packet = broadcast / arp_request
 
-    answered = srp(packet, timeout=2, verbose=False)[0]
+  answered = srp(packet, timeout=2, verbose=False)[0]
 
-    devices = []
+  devices = []
 
-    for sent, received in answered:
-        devices.append({
-            "ip": received.psrc,
-            "mac": received.hwsrc
-        })
+  for sent, received in answered:
+    hostname = get_hostname(received.psrc)
 
-    return devices
+    devices.append(
+        {"ip": received.psrc, "mac": received.hwsrc, "hostname": hostname}
+    )
 
+  return devices
 
 
 def main():
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    network = ipaddress.ip_network(local_ip + '/24', strict=False)
+  hostname = socket.gethostname()
+  local_ip = socket.gethostbyname(hostname)
+  network = ipaddress.ip_network(local_ip + "/24", strict=False)
 
-    print("""
+  print("""
 ███▄    █ ▓█████▄▄▄█████▓ █     █░ ▄▄▄     ▄▄▄█████▓ ▄████▄   ██░ ██ 
  ██ ▀█   █ ▓█   ▀▓  ██▒ ▓▒▓█░ █ ░█░▒████▄   ▓  ██▒ ▓▒▒██▀ ▀█  ▓██░ ██▒
 ▓██  ▀█ ██▒▒███  ▒ ▓██░ ▒░▒█░ █ ░█ ▒██  ▀█▄ ▒ ▓██░ ▒░▒▓█    ▄ ▒██▀▀██░
@@ -40,20 +48,21 @@ def main():
          ░    ░  ░            ░          ░  ░        ░ ░       ░  ░  ░
                                                      ░                  
     """)
-    print("-----------------------------------------------------------------")
-    print(f"Hostname: {hostname}")
-    print(f"Local IP: {local_ip}")
-    print(f"Network: {network}")
-    print()
-    print(f"Scanning network {network} for active devices...")
+  print("-----------------------------------------------------------------")
+  print(f"Hostname: {hostname}")
+  print(f"Local IP: {local_ip}")
+  print(f"Network: {network}")
+  print()
+  print(f"Scanning network {network} for active devices...")
 
-    devices = scan_network(network)
+  devices = scan_network(network)
 
-    for device in devices:
-        print(f"[+] {device['ip']} - {device['mac']}")    
+  for device in devices:
+    print(f"[+] {device['ip']} - {device['mac']} - {device['hostname']}")
 
-    print()
-    print(f"{len(devices)} devices found.")
+  print()
+  print(f"{len(devices)} devices found.")
+
 
 if __name__ == "__main__":
-    main()
+  main()
